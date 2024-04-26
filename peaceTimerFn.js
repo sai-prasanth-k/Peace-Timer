62eb5d7095f8b0d2f4ab65be7c3fc2899da71062
SAI PRASANTH <saiprasanth5293@gmail.com>
2024-04-26 11:38:42 +0530

    Just included another way of builder the peaceTimeerFn

--- peaceTimerFn.js
+++ peaceTimerFn.js
@@ -52,3 +52,55 @@ function oneMinutePeaceTimer() {
     setAndShowTimer();
 }
 oneMinuteBtnElement.addEventListener("click", oneMinutePeaceTimer);
+
+// Method 2
+/* let twentySecondsBtnElement = document.getElementById("twentySecondsBtn");
+let thirtySecondsBtnElement = document.getElementById("thirtySecondsBtn");
+let fortySecondsBtnElement = document.getElementById("fortySecondsBtn");
+let oneMinuteBtnElement = document.getElementById("oneMinuteBtn");
+let timerTextElement = document.getElementById("timerText");
+
+let secondsLeft = 0;
+const timerCompletion = "Your moment is complete";
+let timerId;
+
+function clearPreviousTimers() {
+    clearInterval(timerId);
+}
+
+function startTimer() {
+    if (secondsLeft > 1) {
+        secondsLeft = secondsLeft - 1;
+        timerTextElement.textContent = secondsLeft + " seconds left";
+    }else {
+        clearPreviousTimers();
+        timerTextElement.textContent = timerCompletion;
+    }
+}
+
+function setTimerAndShow() {
+    timerTextElement.textContent = secondsLeft + " seconds left";
+    timerId = setInterval(startTimer,1000);
+}
+
+twentySecondsBtnElement.onclick = function () {
+    secondsLeft = 20;
+    clearPreviousTimers();
+    setTimerAndShow();
+};
+thirtySecondsBtnElement.onclick = function () {
+    secondsLeft = 30;
+    clearPreviousTimers();
+    setTimerAndShow();
+};
+fortySecondsBtnElement.onclick = function () {
+    secondsLeft = 40;
+    clearPreviousTimers();
+    setTimerAndShow();
+};
+oneMinuteBtnElement.onclick = function () {
+    secondsLeft = 60;
+    clearPreviousTimers();
+    setTimerAndShow();
+} */
+
